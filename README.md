056e20b8a9a9c81d665e2af792f4091ce932a385
domschiener <dom@fileyy.com>
2016-12-20 11:43:15 +0100

    Finished function for bundle creation. Added FFI

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ccurl.interface.js
-cCurl Interface for NodeJS
+
+This is a simple node script that makes it possible to interface directly with the [cCurl Library](#https://github.com/iotaledger/ccurl) via Node-FFI. What this is for is basically for doing Proof of Work without IRI Core. Hashing with cCurl is also arguably faster.
+
+## How to use
+
+First you will have to git clone this repo and then install all dependencies (iota.lib.js and ffi):
+```
+$ git clone https://github.com/iotaledger/ccurl.interface.js.git
+$ npm install
+```
+
+After that, you will have to compile cCurl locally on your machine. Follow the instructions on the [official repoe here](#https://github.com/iotaledger/ccurl).
+```
+$ git clone https://github.com/iotaledger/ccurl.git
+$ mkdir build && cd build && cmake .. && cd .. && make -C build
+```
+
+Then copy the `libccurl` file from the `build/lib` folder to the main directory.
---
+++ index.js
@@ -0,0 +1,80 @@
+var IOTA = require('iota.lib.js');
+var ffi = require('ffi')
+
+module.exports = function(minWeightMagnitude, trunkTransaction, branchTransaction, trytes, callback) {
+
+    // TODO: VALIDATE THE INPTUS
+
+    var finalBundleTrytes = [];
+    var previousTxHash;
+
+    // Define libccurl to be used for finding the nonce
+    var libccurl = ffi.Library('./libccurl', {
+      ccurl_pow : [ 'string', [ 'string', 'int'] ]
+    });
+
+    var iota = new IOTA();
+
+
+    // PROCESS LOGIC:
+    // Start with last index transaction
+    // Assign it the trunk / branch which the user has supplied
+    // IF there is a bundle, chain  the bundle transactions via
+    // trunkTransaction together
+    trytes.forEach(function(thisTrytes, num) {
+
+        // If this is the first transaction, to be processed
+        // Make sure that it's the last in the bundle and then
+        // assign it the supplied trunk and branch transactions
+        if (!previousTxHash) {
+
+            var txObject = iota.utils.transactionObject(thisTrytes);
+
+            // Check if last transaction in the bundle
+            if (txObject.lastIndex !== txObject.currentIndex) {
+                return callback(new Error("Wrong bundle order. The bundle should be ordered in descending order from currentIndex"));
+            }
+
+            txObject.trunkTransaction = trunkTransaction;
+            txObject.branchTransaction = branchTransaction;
+
+            var newTrytes = iota.utils.transactionTrytes(txObject);
+
+            // cCurl updates the nonce as well as the transaction hash
+            var returnedTrytes = libccurl.ccurl_pow(newTrytes, minWeightMagnitude);
+
+            var newTxObject= iota.utils.transactionObject(returnedTrytes);
+
+            // Assign the previousTxHash to this tx
+            var txHash = newTxObject.hash;
+            previousTxHash = txHash;
+
+            finalBundleTrytes.push(returnedTrytes);
+
+        } else {
+
+            var txObject = iota.utils.transactionObject(thisTrytes);
+
+            // Chain the bundle together via the trunkTransaction (previous tx in the bundle)
+            // Assign the supplied trunkTransaciton as branchTransaction
+            txObject.trunkTransaction = previousTxHash;
+            txObject.branchTransaction = trunkTransaction;
+
+            var newTrytes = iota.utils.transactionTrytes(txObject);
+
+            // cCurl updates the nonce as well as the transaction hash
+            var returnedTrytes = libccurl.ccurl_pow(newTrytes, minWeightMagnitude);
+
+            var newTxObject= iota.utils.transactionObject(returnedTrytes);
+
+            // Assign the previousTxHash to this tx
+            var txHash = newTxObject.hash;
+            previousTxHash = txHash;
+
+            finalBundleTrytes.push(returnedTrytes);
+        }
+    })
+
+    // reverse the order so that it's ascending from currentIndex
+    return callback(null, finalBundleTrytes.reverse());
+}
